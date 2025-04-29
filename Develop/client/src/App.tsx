e7084fc669bb681bf56166a42f7b308c26febfb2
Mina <TMDHammond@outlook.com>
2025-04-29 13:35:34 -0700

    Added graphql files

--- Develop/client/src/App.tsx
+++ Develop/client/src/App.tsx
@@ -1,14 +1,30 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
 import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
